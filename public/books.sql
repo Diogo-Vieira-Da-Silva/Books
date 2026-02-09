a2371728388018f83ef87e5544bbde92a3fd1817
DIOGO VIEIRA DA SILVA <05708376007@senacrs.edu.br>
2026-02-09 11:26:47 -0300

    Formando o formulário

--- public/books.sql
+++ public/books.sql
@@ -8,8 +8,9 @@ CREATE TABLE books(
     Título VARCHAR(50) NOT NULL,
     Autor VARCHAR(50) NOT NULL,
     gênero varchar(50) not null,
-	Status_Leitura VARCHAR(120) NOT NULL,
-    Nota VARCHAR(30) NOT NULL,
+	Status_Leitura ENUM("Lendo", "Ainda não li", "Finalizado") NOT NULL,
+    Nota ENUM ("0 ESTRELAS", "1 ESTRELA", "2 ESTRELAS", "3 ESTRELAS", "4 ESTRELAS", "5 ESTRELAS")  Not null,
+    Complemento_Nota varchar(50), 
     Ano_de_publicação Date NOT NULL
 );
 
@@ -19,5 +20,4 @@ CREATE TABLE usuario(
     cpf numeric(11) PRIMARY KEY not null,   -- ID manual 100%
     nome_e_sobrenome VARCHAR(50) NOT NULL
     );
-    
    
